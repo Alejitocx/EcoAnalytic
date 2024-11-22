31202ab990cc923f635ae01c87f119d8d75ebc58
Alejitocx <itsalejo12@gmail.com>
2024-11-22 11:12:38 -0500

    PROYECTO CON NAVBAR

--- EcoAnalytic/src/components/NavBar.tsx
+++ EcoAnalytic/src/components/NavBar.tsx
@@ -1,11 +1,36 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/NavBar.css';
+import logo from '../images/logo.png';
 
+const Navbar: React.FC = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-const NavBar = () => {
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">
+          <img src={logo} alt="Logo EcoAnalytic" className="logo-img" />
+        </Link>
+      </div>
 
-export default NavBar
+      <div className="navbar-links-container">
+        <div className={`navbar-links ${isMobileMenuOpen ? 'open' : ''}`}> {/* <-- AQUÍ ESTA EL CAMBIO */}
+          <Link to="/" className="navbar-link">Home</Link>
+          <Link to="/datos-historicos" className="navbar-link">Datos Históricos</Link>
+          <Link to="/dashboard" className="navbar-link">Dashboard</Link>
+        </div>
+      </div>
+
+      <button className="navbar-toggle" onClick={toggleMobileMenu}>
+        ☰
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
